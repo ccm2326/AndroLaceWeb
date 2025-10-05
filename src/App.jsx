299e47d5a83c63f33001fdc686d949216f0c798c
FerTheOnePieceisReal <fernanda.cruz@ucsp.edu.pe>
2025-10-05 12:56:00 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Sidebar from './components/Sidebar';
 import HomePage from './pages/HomePage';
 import PapersPage from './pages/PapersPage';
 import './App.css';
-import Chatbot from "./components/Chatbot";
+
 
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
@@ -25,7 +25,7 @@ function App() {
             <Route path="/papers" element={<PapersPage />} />
           </Routes>
         </main>
-      <Chatbot />
+      
       </div>
     </div>
   );
